7885a6ad56364c933bc378f281d9db23164e44d3
stephaniehohenberg <stephanie.hohenberg@gmail.com>
2025-07-23 08:10:30 -0600

    defines id format for firebase entities

--- backend/gcloudrun/application/firebase_logger.py
+++ backend/gcloudrun/application/firebase_logger.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+import random
 from google.cloud import firestore
 
+def generate_id():
+  date_part = datetime.utcnow().strftime("%Y-%m-%d")
+  random_part = str(random.randint(1000, 9999))  # 4-digit random number
+  return f"{date_part}-{random_part}"
+
 def log_message_to_firebase(message):
+  custom_id = generate_id()
   db = firestore.Client(database='chatbot')
-  db.collection('user-messages').add({
+  db.collection('user-messages').document(custom_id).set({
     'question': message,
     'askedAt': firestore.SERVER_TIMESTAMP
   })
