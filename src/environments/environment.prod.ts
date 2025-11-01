56ad2d829ae25f7a538472ec6e3f4fa826aff385
Continuous Integration <username@users.noreply.github.com>
2025-11-01 16:55:13 +0000

    updates CI/CD Build Artifacts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   sessionizeId: "m90agvqtdd",
   splineId: "AxZheWuU4DZSXNUO",
   chatEndpoint: "https://chatbot-atmljzwjra-ew.a.run.app/ask",
-  featureFlag_chatbotEnabled: "FEATURE_FLAG_CHATBOT_ENABLED",
+  featureFlag_chatbotEnabled: true,
 };
